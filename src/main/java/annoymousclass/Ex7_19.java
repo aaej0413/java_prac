bc351b90e6212851698490c7a498df10da3edf8a
aaej0413 <aaej0413@gmail.com>
2022-07-05 22:29:50 +0900

    익명 클래스 이용 후

---
+++ src/main/java/annoymousclass/Ex7_19.java
@@ -0,0 +1,18 @@
+package annoymousclass;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ex7_19 {
+    public static void main(String[] args) {
+        Button b = new Button("Start");
+        b.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("ActionEvent occurred!!!");
+                }
+            }   // 익명 클래스의 끝
+        );  // main의 끝
+    }
+}
