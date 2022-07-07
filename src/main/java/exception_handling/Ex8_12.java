d61a48a835ac3760a7ef914e9bd679e863c69522
aaej0413 <aaej0413@gmail.com>
2022-07-07 20:23:27 +0900

    예외 던지기

---
+++ src/main/java/exception_handling/Ex8_12.java
@@ -0,0 +1,22 @@
+package exception_handling;
+/*
+예외 던지기 (exception re-throwing)
+ */
+public class Ex8_12 {
+    public static void main(String[] args) {
+        try {
+            method1();
+        } catch (Exception e) {
+            System.out.println("main에서 예외가 처리되었습니다.");
+        }
+    }   // main메서드의 끝
+
+    static void method1() throws Exception {
+        try {
+            throw new Exception();
+        } catch (Exception e) {
+            System.out.println("method1메서드에서 예외가 처리되었습니다.");
+            throw e;    // 다시 예외를 발생시킴.
+        }
+    }   // method1메서드의끝
+}
