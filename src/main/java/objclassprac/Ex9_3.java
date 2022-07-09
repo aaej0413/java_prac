2467e642af2896daa3c35c432ae78fd4b9a040de
aaej0413 <aaej0413@gmail.com>
2022-07-09 18:14:51 +0900

    hashCode() 간단하게 써보기

---
+++ src/main/java/objclassprac/Ex9_3.java
@@ -0,0 +1,17 @@
+package objclassprac;
+
+/*
+hashCode() 간단하게 써보기
+ */
+public class Ex9_3 {
+    public static void main(String[] args) {
+        String str1 = new String("abc");
+        String str2 = new String("abc");
+
+        System.out.println(str1.equals(str2));  // false일 것 같지만 true.
+        System.out.println(str1.hashCode());
+        System.out.println(str2.hashCode());
+        System.out.println(System.identityHashCode(str1));
+        System.out.println(System.identityHashCode(str2));
+    }
+}
