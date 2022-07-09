b5842eeb8277c5693bff65c24eab0c7a41008804
aaej0413 <aaej0413@gmail.com>
2022-07-09 18:16:53 +0900

    join()과 StringJoiner

---
+++ src/main/java/objclassprac/Ex9_9.java
@@ -0,0 +1,24 @@
+package objclassprac;
+
+import java.util.Arrays;
+import java.util.StringJoiner;
+
+/*
+join()과 StringJoiner
+ */
+public class Ex9_9 {
+    public static void main(String[] args) {
+
+        String animals = "dog,cat,bear";
+        String[] arr = animals.split(",");
+
+        System.out.println(String.join("-",arr));
+
+        StringJoiner sj = new StringJoiner("/", "[", "]");
+
+        for(String s : arr) {
+            sj.add(s);
+        }
+        System.out.println(sj.toString());
+    }
+}
