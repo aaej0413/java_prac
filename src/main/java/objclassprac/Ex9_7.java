0e16b890adb2afa34dd767f445a98d99c94019a7
aaej0413 <aaej0413@gmail.com>
2022-07-09 18:15:51 +0900

    문자열 리터럴(String 리터럴)

---
+++ src/main/java/objclassprac/Ex9_7.java
@@ -0,0 +1,12 @@
+package objclassprac;
+/*
+문자열 리터럴(String리터럴)
+ */
+public class Ex9_7 {
+    public static void main(String[] args) {
+        String s1 = "AAA";
+        String s2 = "AAA";
+        String s3 = "AAA";
+        String s4 = "BBB";
+    }
+}
